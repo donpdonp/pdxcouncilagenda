82cabfca32247c9b49b8aedf2897551b182d911c
dp <donp@commons.gk.donp.org>
2022-01-10 10:58:06 -0800

    2022 updates

--- scrape.rb
+++ scrape.rb
@@ -5,99 +5,59 @@ require 'open-uri'
 require 'json'
 require 'time'
 
-def assume_date(tablename)
-  dayword =  case tablename
-  when 'bluetable'
-    'wednesday'
-  when 'greentable'
-    'wednesday'
-  when 'goldtable'
-    'thursday'
-  end
-  t = Time.now
-  7.times do 
-    break if t.public_send(dayword+'?')
-    t += 60*60*24
-  end
-  t
-end
+url = ARGV[0] || "https://www.portland.gov/council/agenda" 
+Uri = URI::parse(url)
+STDERR.puts Uri
 
-def parse_bill(agenda_row, agenda_date, uri)
+def parse_bill(agenda_row)
   bill = {}
-  title = agenda_row.text.gsub(/[[:space:]]/, ' ').strip #replace non-breaking spaces
-  agenda_number_match = title.match(/^(\s*(\*)?(\d+)(\s+TIME.CERTAIN:.(\d+:\d+.\w\w).)?)/)
-  agenda_number = agenda_number_match[3]
-  emergency = !!agenda_number_match[2]
-  if agenda_number_match[5]
-    item_date = Time.parse("#{agenda_date.to_date} #{agenda_number_match[5]}")
-    bill.merge!(:time_certain => item_date)
+  agenda_row.css('h4').each do |id|
+    number = id.text.strip
+    STDERR.puts("id #{number}")
+    bill['number'] = number
+    break
   end
-  title = title[agenda_number_match[1].length + 2, title.length]
-
-  bill.merge!( :number => agenda_number,
-              :session => agenda_date,
-              :title => title)
-  citypdf = agenda_row.css('strong a')[0]
-  bill.merge!(:link => "https://#{uri.host}/#{citypdf.attributes['href']}") if citypdf
-  bill.merge!({:emergency => true}) if emergency
+  agenda_row.css('div.council-document__title').each do |id|
+    parts = id.text.split("\n")
+    title = parts[0].strip
+    bill['title'] = title
+    STDERR.puts("title #{title}")
+    dept = parts[1].strip
+    STDERR.puts("dept #{dept}")
+    path = id.css('a').attr('href')
+    link = "https://#{Uri.host}#{path}"
+    bill['link'] = link
+    STDERR.puts("link #{link}")
+  end
+  # bill.merge!(:time_certain => item_date)
+  # bill.merge!(:link => "https://#{uri.host}/#{citypdf.attributes['href']}") if citypdf
+  # bill.merge!({:emergency => true}) if emergency
   bill
 end
 
-def tableread(tablerow, uri, agenda_date) 
+def tableread(tablerow, agenda_date) 
   items = []
-  tablerow.css("tbody tr").each do |row|
-    row.css('td').each do |head|
-      unless agenda_date
-        date_match = head.text.match(/\d+:\d+ \w\w, \w+ [\d-]+, 20\d\d/)
-        if date_match
-          clean_date = date_match[0].gsub(/-\d+/, '')
-          agenda_date = Time.parse(clean_date)
-        else
-          #agenda_date = assume_date(tablename)
-          #STDERR.puts "parse warning: table row header #{agenda_date.inspect} has no parsable date"
-        end
-      end
-    end
-
-    if agenda_date
-      p = row.css('p').first
-      if p && (p.css('strong').length > 0)
-        bill = parse_bill(p, agenda_date, uri)
+  tablerow.css("div.view-admin-agenda-items").each do |row|
+    row.css('div.relation--type-agenda-item').each do |item|
+        bill = parse_bill(item)
+        bill['session'] = Time.parse(agenda_date).localtime
         items << bill if bill
-      end
     end
   end
   items
 end
 
-def datefind(text)
-  match = text.match(/(\w+ \d+)([-\d]+)?(, 20\d\d)/)
-  if match
-    month_day = "#{match[1]}#{match[3]}"
-    Date.parse(month_day)
-  end
-end
-
-url = ARGV[0] || 'https://www.portlandoregon.gov/auditor/index.cfm?c=26997'
-uri = URI::parse(url)
-STDERR.puts uri
 doc = Nokogiri::HTML(URI.open(url).read)
-agenda_date = nil
-
-doc.css("h2.content-center").each do |row|
-  agenda_date ||= datefind(row.text)
-end
-STDERR.puts "Agenda Date: #{agenda_date}"
 
 items = []
-doc.css("section#main-content table").each do |row|
-  header_text = row.css('tr:first-child h2').text
-  STDERR.puts "Section: #{header_text}"
-  header_date = datefind(header_text)
-  classTableName = row.attributes['class']
-  tableitems = tableread(row, uri, header_date)
-  STDERR.puts "parsing table.#{classTableName} -> #{tableitems.length} items found"
-  items += tableitems
+doc.css("div.relation--type-council-session").each do |row|
+  agenda_date = row.css('div.session-meta time').attr('datetime').text
+  if agenda_date then
+    STDERR.puts "Section: #{agenda_date}"
+    tableitems = tableread(row, agenda_date)
+    STDERR.puts "parsing section #{agenda_date} -> #{tableitems.length} items found"
+    items += tableitems
+  end
 end
 
 agenda = {
